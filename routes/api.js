f1cb16384c42ca62ef1eaf2c6cac8f2087d2a2f8
ucup233 <sufhidayat9@gmail.com>
2025-11-26 11:07:41 +0700

    pembuatan file api routes

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const komikController = require("../controllers/komikController");
+const upload = multer({ dest: "uploads/" });
+
+// Routes for Komik
+router.post("/komik", upload.single("image"), komikController.createKomik);
+router.get("/komik", komikController.getAllKomik);
+router.get("/komik/:id", komikController.getKomikById);
+router.put("/komik/:id", upload.single("image"), komikController.updateKomik);
+router.delete("/komik/:id", komikController.deleteKomik);
+
+module.exports = router;
