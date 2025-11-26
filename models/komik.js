591b684ebbc48faf999c205505a46ac0b08ab57f
ucup233 <sufhidayat9@gmail.com>
2025-11-26 11:02:20 +0700

    pembuatan models komik

---
+++ models/komik.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const Komik = sequelize.define(
+    "Komik",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      judul: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      penulis: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      deskripsi: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      imageType: DataTypes.STRING,
+      imageName: DataTypes.STRING,
+      imageData: DataTypes.BLOB('long'),
+      
+    },
+    {
+      tableName: "Komik",
+    }
+  );
+  return Komik;
+};
